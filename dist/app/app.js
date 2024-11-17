4e6c31fde36bd15e1dc3d46f8df0307c90c52a59
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-11-17 20:21:53 +0600

    make global error handler

--- dist/app/app.js
+++ dist/app/app.js
@@ -3,19 +3,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const express = require('express')
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 //parsers
 app.use(express_1.default.json());
+// router
+const userRouter = express_1.default.Router();
+app.use('/api/v1/users/', userRouter);
 //middleware
 const logger = (req, res, next) => {
     console.log('url', req.url, 'method', req.method, 'hostname', req.hostname);
     next();
 };
-app.get('/', logger, (req, res) => {
-    res.send('Hello Dev from, another world ! i am using nodemon');
+userRouter.get('/create-user', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: 'User is created Successfully',
+        data: user
+    });
+});
+app.get('/', logger, (req, res, next) => {
+    try {
+        res.send(responsed);
+    }
+    catch (err) {
+        next(err);
+    }
 });
 app.post('/', (req, res) => {
     console.log(req.body);
@@ -23,5 +39,14 @@ app.post('/', (req, res) => {
         status: 200,
         message: 'got data'
     });
+});
+//global error
+app.use((error, req, res, next) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: 'Dur sala mor !'
+        });
+    }
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,46 @@
+import { NextFunction } from './../../node_modules/@types/express-serve-static-core/index.d';
+
+
 // const express = require('express')
-import express, { NextFunction, Request, Response } from 'express';
+import { error } from 'console';
+import express, { NextFunction, Request, response, Response } from 'express';
 const app = express()
 const port = 3000
 
 //parsers
 app.use(express.json())
 
+
+
+
+
+// router
+const userRouter = express.Router()
+
+
+app.use('/api/v1/users/', userRouter)
 //middleware
 const logger = (req:Request, res:Response, next:NextFunction) => {
     console.log('url',req.url, 'method',req.method, 'hostname',req.hostname);
    next() 
 }
 
-app.get('/',logger, (req:Request, res:Response) => {
-  res.send('Hello Dev from, another world ! i am using nodemon')
+userRouter.get('/create-user', (req: Request, res: Response) => {
+    const user = req.body
+    console.log(user);
+    res.json({
+        success: true,
+        message: 'User is created Successfully',
+        data:user
+    })
+})
+
+app.get('/',logger, (req:Request, res:Response,next:NextFunction) => {
+    try {
+     res.send(responsed)
+    } catch (err) {
+        next(err)
+ }
 })
 
 app.post('/', (req:Request, res:Response) => {
@@ -24,4 +51,14 @@ app.post('/', (req:Request, res:Response) => {
     })
 })
 
+
+//global error
+app.use((error:any, req:Request, res:Response, next:NextFunction) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message:'Dur sala mor !'
+       })
+   }
+})
 export default app;
